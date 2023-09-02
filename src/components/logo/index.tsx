5fce1e2075b720ba773174a8d1d664e26e7603ec
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-02 03:19:15 +0100

    feat: add footer

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,10 +3,15 @@ import Image from 'next/image'
 import React from 'react'
 import logo from "@/assets/images/logo.png";
 
-export const Logo = () => {
+export type Props = {
+  className?: string;
+  width?: number;
+};
+export const Logo:React.FC<Props> = ({width, className}) => {
+  const getWidth = width!==0 && width!== undefined? width: 280
   return (
     <div>
-         <Image src={logo} width={280} alt="" />
+         <Image src={logo} width={getWidth} alt="" className={className}/>
     </div>
   )
 }
