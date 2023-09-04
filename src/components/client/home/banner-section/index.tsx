62880f0904fd9ceab6949589d97758ec6a740f07
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-04 13:00:15 +0100

    refactor:update navigation

--- src/components/client/home/banner-section/index.tsx
+++ src/components/client/home/banner-section/index.tsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <div className="grid grid-cols-12 sm:grid-cols-2">
           <div className="">
             <div className="flex flex-col gap-7">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-6xl md:leading-tight ">
+              <h1 className="font-bold tracking-tight text-gray-800 sm:text-6xl md:leading-tight ">
               Acceptez les paiements mobiles sur votre site web et application mobile.
               </h1>
                <div>
