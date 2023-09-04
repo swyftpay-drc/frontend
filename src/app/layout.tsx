7b44b6f42b9239384f4c6bb8ff79d5e921a2c917
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-04 12:55:15 +0100

    refactor: add Montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import Provider from './provider/Provider'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
