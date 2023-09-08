bcaeb4720ad0b38b8cb98b1c0624b1750997444e
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-08 14:43:15 +0100

    feat:add overflow-hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Home } from "@/components/client/home";
 export default function HomePage() {
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white overflow-hidden">
     <Home/>
     </main>
   );
