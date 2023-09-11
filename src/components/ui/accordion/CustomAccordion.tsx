df18b2ce016153888635d4e352d4e8784f4f962f
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-09-11 19:10:23 +0100

    refactor:update accordion

--- src/components/ui/accordion/CustomAccordion.tsx
+++ src/components/ui/accordion/CustomAccordion.tsx
@@ -21,9 +21,9 @@ export const CustomAccordion: React.FC<Props> = ({title, content, defaultOpen, c
         <div className={`w-full border transition-transform duration-500 ease-in-out my-3 rounded-2xl ${ isOpen?'bg-white/90 border-0 shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] mb-12':'bg-transparent'}  `}>
             <button
                 onClick={toggleAccordion}
-                className="w-full flex justify-between items-center px-4 py-2 text-text-primary font-bold text-2xl focus:outline-none"
+                className="w-full flex justify-between items-center px-4 py-2 focus:outline-none"
             >
-                <span>{title}</span>
+                <span className="text-text-primary font-bold xl:text-2xl text-left ">{title}</span>
                 <span className="transition-transform transform duration-500">
           {isOpen ? (
               <IconMinus id={1} open={isOpen} className='text-primary-900 transition-transform duration-200 ease-in-out motion-reduce:transition-none'/>
